33d76a2ddeb37a9326e38d6ea89a1c017d46af53
Frankillercloud <98995931+Frankillercloud@users.noreply.github.com>
2023-03-25 10:10:12 +1100

    Update and rename config.py.example to config.py

--- src/config.py
+++ src/config.py
@@ -30,4 +30,4 @@ REDIS_HOST = None
 REDIS_PORT = 6379
 REDIS_PASSWORD = None
 
-TOKEN = None # discord bot token
+TOKEN = MTA1ODIxODI4NDEwMjcxMzM2NA.GXgyd3.IZxKbz_fR_0-dTmSCLFGsqTSqPjNLDOmxY8WgA # discord bot token
